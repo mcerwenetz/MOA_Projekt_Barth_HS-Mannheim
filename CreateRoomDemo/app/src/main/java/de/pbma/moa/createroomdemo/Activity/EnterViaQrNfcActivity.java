e1e2d740ed67ee03976b1d53a49a754463ba9b0a
Raphael Barth <barthra@web.de>
2020-12-31 11:01:07 +0100

    neue klasse für raum beitritt erstellt

---
+++ CreateRoomDemo/app/src/main/java/de/pbma/moa/createroomdemo/Activity/EnterViaQrNfcActivity.java
@@ -0,0 +1,20 @@
+package de.pbma.moa.createroomdemo.Activity;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterViaQrNfcActivity  extends AppCompatActivity {
+
+
+
+
+    private void btnQrClicked(View v){
+        
+    }
+
+    //TODO @marius dein Spielplatz
+    private void btnNfcClicked(View v){
+
+    }
+}
