8302f63605396d65b13cf9feff7f549a1e459a37
mike <1725050@stud.hs-mannheim.de>
2021-01-13 20:44:28 +0100

    Objekt Factory (zum Teil )

--- CreateRoomDemo/app/src/main/java/de/pbma/moa/createroomdemo/AdapterJsonMqtt.java
+++ CreateRoomDemo/app/src/main/java/de/pbma/moa/createroomdemo/AdapterJsonMqtt.java
@@ -9,20 +9,55 @@ import de.pbma.moa.createroomdemo.database.ParticipantItem;
 import de.pbma.moa.createroomdemo.database.RoomItem;
 
 public class AdapterJsonMqtt {
+    private ParticipantItem participantItem;
 
     public static JSONObject msgToJson(String msg){
 
     }
 
     public static RoomItem createRoom(JSONObject jsonObject){
-        return null;
+        RoomItem roomItem = new RoomItem();
+        //Auslesen der JsonFiles
+        String roomName = (String)jsonObject.get("roomName");
+        boolean open     = (boolean)jsonObject.get("open");
+        String host     = (String)jsonObject.get("host");
+        String eMail    = (String)jsonObject.get("host");
+        String phone    = (String)jsonObject.get("eMail");
+        String place    = (String)jsonObject.get("place");
+        String address  = (String)jsonObject.get("address");
+        String extra    = (String)jsonObject.get("extra");
+
+        long startTime  = (long)jsonObject.get("startTime");
+        long endTime    = (long)jsonObject.get("endTime");
+
+        roomItem.createRoom(roomName,open,host,eMail,phone,place,address,extra,startTime,endTime);
+        //TODO Fremdid nochmal nachfragen wie der Gedanke dahinter ist
+        return roomItem;
     }
 
     public static ParticipantItem createParticipant(JSONObject jsonObject){
-        return null;
+        ParticipantItem participantItem = new ParticipantItem();
+        //Auslesen der JsonFiles
+        String name     = (String)jsonObject.get("Name");
+        String extra    = (String)jsonObject.get("extra");
+
+        String eMail    = (String)jsonObject.get("extra");
+        String phone    = (String)jsonObject.get("extra");
+
+        //raumid von Teiler
+        long  roomid    = (long)jsonObject.get("roomId");
+        long  enterTime = (long)jsonObject.get("enterTime");
+//        long  exitTime  = (long)jsonObject.get("exitTime");
+
+        //Uebergeben der Items an die
+        participantItem.createParticipant(name,extra,eMail,phone,roomid,enterTime);
+        return participantItem;
     }
 
     public static List<ParticipantItem> createParticipantList(JSONObject jsonObject){
         return null;
     }
+
 }
+
+
